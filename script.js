38f23e062e0c6fefd4d4f42c15b6e718491e06c8
DevSwaika99 <66863974+DevSwaika99@users.noreply.github.com>
2021-01-17 23:48:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.querySelector(".search-btn").addEventListener('click',searchText);
+document.querySelector(".cancel").addEventListener('click',clearText);
+document.querySelector('.input').addEventListener('keyup',function(event){
+    if (event.keyCode === 13) {
+   event.preventDefault();
+   document.querySelector(".search-btn").click();
+  }
+});
+
+function searchText(){
+    output=""
+    var search=document.querySelector('.input').value.toUpperCase();
+    const adg = `http://adgrecruitments.herokuapp.com/user/getResult?regno=${search}`;
+    fetch(adg)
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+        if (data.message=='User does Not Exist'){
+            if (search==''){
+                output+=`<h1 class="nfound">Please enter a Registration number<h1>`;
+            }
+            else{
+                output+=`<h1 class="nfound">${data.message}<h1>`;
+            }
+        }
+        else if (data.message=='Hey, you missed it by a narrow margin. All the best for future endeavours!'){
+            output+=`<h1 class="found_1">${data.message}<h1>`;
+        }
+        else if (data.message=='Hey, you missed it by a narrow margin. See you next year. Till then keep hustling.'){
+            output+=`<h1 class="found_1">${data.message}<h1>`;
+        }
+        else {
+            output+=`<div class="container info">      
+            <p>
+                ${data.message}
+            </p> 
+            </div>`;
+        }
+        document.querySelector('.output').innerHTML=output;
+
+    });
+}
+
+function clearText(){
+    document.querySelector('.output').innerHTML='';
+    document.querySelector('.input').value='';
+}
